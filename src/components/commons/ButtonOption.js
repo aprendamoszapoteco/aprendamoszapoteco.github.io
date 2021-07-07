800afa651c83a6571cc916551c3c8c6f0b28ab3b
Eleomar pedro lorenzo <eleomarpedrolorenzo@hotmail.com>
2021-07-07 14:14:43 -0500

    Added ButtonOption

---
+++ src/components/commons/ButtonOption.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import '../../styles/styleButtonOption.css';
+
+const ButtonOption = ({ operatingSystem, suportedVersions, href, isLittle = false }) => {
+  return (
+    <a
+      target="_blank"
+      href={ href }
+      rel="noreferrer"
+      className="button-option btn d-flex flex-wrap flex-column justify-content-center py-2"
+      style={ {padding: isLittle ? '0 3rem' : '0 5rem'} }
+    >
+      <p style={ {fontSize: '1.2rem'} }>
+        <i className="bi bi-download"></i><span className="px-3">{ operatingSystem }</span>
+      </p>
+      <small className="small-letter">{ suportedVersions }</small>
+    </a>
+  );
+};
+
+ButtonOption.propTypes = {
+  operatingSystem: PropTypes.string.isRequired,
+  suportedVersions: PropTypes.string.isRequired,
+  href: PropTypes.string.isRequired,
+  isLittle: PropTypes.bool
+};
+
+export default ButtonOption;
