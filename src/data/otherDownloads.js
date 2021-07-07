90fa2b887677b50125b9e2f3c89e6907914290ff
Eleomar pedro lorenzo <eleomarpedrolorenzo@hotmail.com>
2021-07-07 18:28:47 -0500

    OtherDownloads section added

---
+++ src/data/otherDownloads.js
@@ -0,0 +1,14 @@
+export const otherDownloads = [
+  {
+    nameToImage: 'windows',
+    operatingSystem: ['Windows'],
+    suportedVersions: ['Windows 7, 8, 10'],
+    linksToDownload: ['']
+  },
+  {
+    nameToImage: 'linux',
+    operatingSystem: ['.deb', '.rpm'],
+    suportedVersions: ['Debian, Ubuntu', 'Fedora, Red Hat, SUSE'],
+    linksToDownload: ['', '']
+  }
+];
