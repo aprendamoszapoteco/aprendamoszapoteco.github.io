8d08a940b8d9c9064b2266918c40afb350de186a
Eleomar pedro lorenzo <eleomarpedrolorenzo@hotmail.com>
2021-07-08 17:16:25 -0500

    Added and initial configuration for SEO

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Suspense } from 'react';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import Helmet from 'react-helmet';
 
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -22,15 +23,36 @@ const App = () => {
       <main>
         <Switch>
           <Route exact path="/">
+            <Helmet>
+              <title>Inicio | Aprendamos Zapoteco del Sur</title>
+              <meta
+                name="description"
+                content="Conoce, descarga e instala esta aplicación de escritorio denominada Aprendamos Zapoteco, para fomentar esta hermosa lengua, así como su identidad cultural."
+              />
+            </Helmet>
             <Home />
           </Route>
           <Route path="/contenido">
             <Suspense fallback={ <SpinnerLoading /> }>
+              <Helmet>
+                <title>Contenido | Aprendamos Zapoteco del Sur</title>
+                <meta
+                  name="description"
+                  content="Consulta las secciones generales, así como una breve descripción, con las que cuenta esta aplicación de escritorio denominada Aprendamos Zapoteco del Sur."
+                />
+              </Helmet>
               <Content />
             </Suspense>
           </Route>
           <Route path="/acerca-de">
             <Suspense fallback={ <SpinnerLoading /> }>
+              <Helmet>
+                <title>Acerca de | Aprendamos Zapoteco del Sur</title>
+                <meta
+                  name="description"
+                  content="Descubre el lugar de origen de esta aplicación de escritorio para aprender zapoteco, así como también algunos de sus participantes, y también información sobre sus desarrolladores."
+                />
+              </Helmet>
               <About />
             </Suspense>
           </Route>
