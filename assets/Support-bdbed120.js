3a55a48731cc0204680c6b4be4a2bd916d10df90
Eleomar Pedro Lorenzo <eleomarpedrolorenzo@gmail.com>
2023-11-03 22:42:48 -0600

    Updates

---
+++ assets/Support-bdbed120.js
@@ -0,0 +1,10 @@
+import{s as a,j as e}from"./index-43adff1f.js";import{S as r}from"./Subtitle-3e6a4bf2.js";const n=["aprendamoszapoteco@gmail.com","eleomarpedrolorenzo@gmail.com","galvantorres2410@gmail.com","martin.monjaraz.almaraz@gmail.com"],l=a.a`
+  color: #0068BC;
+  text-align: center;
+  text-decoration: none;
+  cursor: pointer;
+
+  &:hover {
+    text-decoration: underline;
+  }
+`,c=()=>e.jsxs("div",{className:"p-3",style:{backgroundColor:"#DCEEF2"},id:"support",children:[e.jsx(r,{children:"¿No te funciono el enlace?"}),e.jsx("p",{children:"Comunícate bajo los siguientes enlaces:"}),e.jsx("ul",{style:{listStyleType:"none",padding:0},children:n.map((o,t)=>e.jsx("li",{className:"text-center mb-2",children:e.jsx(l,{href:`mailto:${o}`,children:o})},t))})]});export{c as default};
