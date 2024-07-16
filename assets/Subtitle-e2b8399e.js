bd704584d3ee24e41ff535e29ef8e88cc54db2dd
EleomarPL <EleomarPL@users.noreply.github.com>
2024-07-16 05:04:50 +0000

    deploy: 4134e7fe00d13d1e98c80f6c8bf8ea336f5c5705

---
+++ assets/Subtitle-e2b8399e.js
@@ -0,0 +1,1 @@
+import{P as s,j as r}from"./index-6badcd11.js";const t=({children:e})=>r.jsx("h4",{className:"text-center fw-bold fs-5 mb-3",children:e});t.propTypes={children:s.node.isRequired};export{t as S};
---
+++ assets/Support-dc36efdf.js
@@ -0,0 +1,10 @@
+import{s as n,j as e}from"./index-6badcd11.js";import{S as s}from"./Subtitle-e2b8399e.js";const r=["aprendamoszapoteco@gmail.com"],a=n.a`
+  color: #0068BC;
+  text-align: center;
+  text-decoration: none;
+  cursor: pointer;
+
+  &:hover {
+    text-decoration: underline;
+  }
+`,i=()=>e.jsxs("div",{className:"p-3",style:{backgroundColor:"#DCEEF2"},id:"support",children:[e.jsx(s,{children:"¿No te funciono el enlace?"}),e.jsx("p",{children:"Comunícate bajo los siguientes enlaces:"}),e.jsx("ul",{style:{listStyleType:"none",padding:0},children:r.map((t,o)=>e.jsx("li",{className:"text-center mb-2",children:e.jsx(a,{href:`mailto:${t}`,children:t})},o))})]});export{i as default};
